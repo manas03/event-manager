22287c0cc2da14cf99c774c3597bbb8935c2de89
manas03 <03.msahai03@gmail.com>
2020-11-03 22:25:37 +0530

    minor change

--- eventorganiser/src/Components/Landing.js
+++ eventorganiser/src/Components/Landing.js
@@ -6,16 +6,16 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <div className="landing-inner text-light">
+        <div className="landing-inner dark-overlay text-light">
           <div className="container">
             <div className="row">
-              <div className="col-md-12 text-center text-dark">
+              <div className="col-md-12 text-center ">
                 <h1 className="display-3 mb-4">Event Manager</h1>
                 <p className="lead pt-2">
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio eveniet vel dignissimos facilis quod culpa tenetur incidunt officia necessitatibus odit. Perferendis explicabo veniam doloremque rem illo assumenda, quibusdam ducimus deserunt.
                 </p>
                 <p className="lead pt-2 pb-4">
-                  <Link to="/register" className="text-decoration-none text-dark text-monospace">Create an account</Link> as an Organiser or a Student, or log in if you have already have an account
+                  <Link to="/register" className="text-white font-weight-bolder text-monospace">Create an account</Link> as an Organiser or a Student, or log in if you have already have an account
                 </p>
                 <Link to="/login" className="btn btn-lg btn-dark mr-2">
                   Log In Organiser
