6905b9556983af25b3113d2f87bb06308886a917
manas03 <03.msahai03@gmail.com>
2020-11-03 21:31:13 +0530

    added fest card in dashboard

--- eventorganiser/src/Components/Dashboard.js
+++ eventorganiser/src/Components/Dashboard.js
@@ -4,7 +4,26 @@ import { Link } from 'react-router-dom'
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="row mx-auto">
+        <div className="container w-25 pt-5">
+          <Link className="text-decoration-none" to="/addevent">
+          <div className="wrapper">            
+              <div className="card">
+                <h4>Organise A Fest</h4>
+                <h1>
+                  <span className="enclosed">
+                    ADD
+                  </span>
+                  Fest
+                </h1>
+              </div>
+          </div>
+          </Link>
+          <p className="py-3 display-5 lead pt-5 text-center">
+            Wanna to organise an fest in your institution? Tap on the button above. To organise a fest you must belong to an organisation. After creating a fest you need to add the events in your fest where you can manage them.
+          </p>
+        </div>      
         <div className="container w-25 pt-5">
           <Link className="text-decoration-none" to="/addevent">
           <div className="wrapper">            
@@ -19,10 +38,11 @@ export default class Dashboard extends Component {
               </div>
           </div>
           </Link>
-          <p className="p-3 display-5 lead pt-5 text-center">
+          <p className="py-3 display-5 lead pt-5 text-center">
             If you want to organise an event tap on the button above. To organise an event you must belong to an organisation and you need to enter all your profile details.
           </p>
-        </div>      
+        </div>
+      </div>
       </div>
     )
   }
